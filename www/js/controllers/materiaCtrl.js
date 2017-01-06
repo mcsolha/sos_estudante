a1951eac4e7ae9b6f47a4fac50a66e76286cc0ab
marianadias <marianaapdias96@gmail.com>
2017-01-06 16:37:00 -0200

    Modal de dados da materia estruturado

--- www/js/controllers/materiaCtrl.js
+++ www/js/controllers/materiaCtrl.js
@@ -1,9 +1,26 @@
 angular.module('sos_estudante.controllers')
 
-.controller('matRiasCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('matRiasCtrl', ['$scope', '$stateParams', '$ionicModal', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $ionicModal) {
 
+  //MODAL DA NOVA MATÉRIA
+    $ionicModal.fromTemplateUrl('../templates/dadosMaterias.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.modal = modal;
+      });
+      //ABRE MODAL
+    $scope.openModal = function() {
+      $scope.modal.show();
+    };
+    //FECHA MODAL
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    };
 
-}])
+  }
+
+])
