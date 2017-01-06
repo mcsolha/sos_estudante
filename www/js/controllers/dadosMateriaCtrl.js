73335f3ecbf4d3cc700dd02a6e7e6bea6604d5e7
marianadias <marianaapdias96@gmail.com>
2017-01-06 08:54:11 -0200

    arquivos de controller e style dadosMateria criados

---
+++ www/js/controllers/dadosMateriaCtrl.js
@@ -0,0 +1,9 @@
+angular.module('sos_estudante.controllers')
+
+.controller('dadosMateriaCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+    
+
+}])
