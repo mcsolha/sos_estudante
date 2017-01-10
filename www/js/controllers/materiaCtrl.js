828b37ca94aba6f12333b24886ea803c317d0782
Matheus Solha <mcsolha@gmail.com>
2017-01-10 20:20:47 -0200

    Nova materia ajustada

--- www/js/controllers/materiaCtrl.js
+++ www/js/controllers/materiaCtrl.js
@@ -1,8 +1,58 @@
 angular.module('sos_estudante.controllers')
-.controller('matRiasCtrl', ['$scope', '$stateParams', '$ionicModal', '$ionicPopup',  // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('matRiasCtrl', ['$scope', '$stateParams', '$ionicModal', '$ionicPopup', 'ionicTimePicker', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, $ionicModal, $ionicPopup) {
+function ($scope, $stateParams, $ionicModal, $ionicPopup, ionicTimePicker) {
+  //Objeto a ser mandado para o banco de dados
+  $scope.data = {};
+
+  // Função utilizada para formatar o numero
+  function FormatarNumero(num, length) {
+    var r = "" + num;
+    while (r.length < length) {
+        r = "0" + r;
+    }
+    return r;
+  }
+
+  var timePickerObj = {
+    inputTime: 50400,   //Optional
+    format: 24,         //Optional
+    step: 5,           //Optional
+    setLabel: 'Definir',    //Optional
+    closeLabel: 'Cancelar'
+  };
+
+  // Função que chama o seletor de tempo para armazenar hora inicial
+  $scope.horaIni = function() {
+    timePickerObj.callback = function(val) {
+      if (typeof (val) === 'undefined') {
+        console.log('Time not selected');
+      } else {
+        // console.log(val);
+        var tempoSelecionado = new Date(val * 1000);
+        // console.log('Selected epoch is : ', val, 'and the time is ', tempoSelecionado.getUTCHours(), 'H :', tempoSelecionado.getUTCMinutes(), 'M');
+
+        $scope.data.horaIniSelected = FormatarNumero(tempoSelecionado.getUTCHours(),2) + ':' + FormatarNumero(tempoSelecionado.getUTCMinutes(),2);
+      }
+    }
+    ionicTimePicker.openTimePicker(timePickerObj);
+  }
+
+  // Função que chama o seletor de tempo para armazenar hora final
+  $scope.horaFin = function() {
+    timePickerObj.callback = function(val) {
+      if (typeof (val) === 'undefined') {
+        console.log('Time not selected');
+      } else {
+        var tempoSelecionado = new Date(val * 1000);
+        // console.log('Selected epoch is : ', val, 'and the time is ', tempoSelecionado.getUTCHours(), 'H :', tempoSelecionado.getUTCMinutes(), 'M');
+        $scope.data.horaFinSelected = FormatarNumero(tempoSelecionado.getUTCHours(),2) + ':' + FormatarNumero(tempoSelecionado.getUTCMinutes(),2);;
+      }
+    }
+    ionicTimePicker.openTimePicker(timePickerObj);
+  }
+
   $scope.diasSemana = [{},{},{},{},{},{}];
   for(i=0;i<6;i++){
    $scope.diasSemana[i].class = "button-stable button-outline";
