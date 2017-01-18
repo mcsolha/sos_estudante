2ffd51ec59ea8e46f6f0b62f8f6f2ad9c91d6be5
Mariana Dias <marianaapdias96@gmail.com>
2017-01-18 13:52:43 -0200

    controler do dados materia na materiaCtrl

--- www/js/controllers/materiaCtrl.js
+++ www/js/controllers/materiaCtrl.js
@@ -1,8 +1,8 @@
 angular.module('sos_estudante.controllers')
-.controller('matRiasCtrl', ['$scope', '$stateParams', '$ionicModal', '$ionicPopup', 'ionicTimePicker', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('matRiasCtrl', ['$scope', '$stateParams', '$ionicModal', '$ionicPopup', 'ionicTimePicker','$ionicPopover', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, $ionicModal, $ionicPopup, ionicTimePicker) {
+function ($scope, $stateParams, $ionicModal, $ionicPopup, ionicTimePicker, $ionicPopover) {
   //Objeto a ser mandado para o banco de dados
   $scope.data = {};
 
@@ -122,6 +122,48 @@ function ($scope, $stateParams, $ionicModal, $ionicPopup, ionicTimePicker) {
   ///////FIM POPUP
 
 
+//////////////////////////Inicio Dados Materia////////////////////////////////
+//Inicio MODAL DA estimativas
+ $ionicModal.fromTemplateUrl('./templates/estimativas.html', {
+     scope: $scope,
+     animation: 'slide-in-up'
+   }).then(function(modal) {
+     $scope.modal = modal;
+   });
+   //ABRE MODAL
+ $scope.openModalEst = function() {
+   $scope.modal.show();
+ };
+ //FECHA MODAL
+ $scope.closeModalEst = function() {
+   $scope.modal.hide();
+ };
+ //Fim modal Estimativas
+
+ //Inicio POPUP Faltas
+ $scope.onshowPopUpFaltas = function(){
+   $scope.maisFaltas = 1;
+   $ionicPopup.show({
+     title: 'Incluir Faltas',
+     templateUrl:'./templates/faltasPopup.html',
+     scope: $scope,
+     buttons:[
+       {text: "Salvar",
+       type: 'button-dark'},
+       {text: "Cancelar"}
+     ],
+   });
+ }
+ //Fim POPup faltas
+
+ //Começo do popOver
+ $ionicPopover.fromTemplateUrl('./templates/dadosMatPopOver.html', {
+   scope: $scope,
+ }).then(function(popover) {
+   $scope.popoverDados = popover;
+ });
+ //Fim do popOver
+//////////////////////////Fim Dados Materia////////////////////////////////
 }
 
 ])
